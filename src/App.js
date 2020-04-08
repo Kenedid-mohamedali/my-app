e575ed61f427eba1dd218415c2c745c93fad5057
Kenedid-mohamedali <60650399+Kenedid-mohamedali@users.noreply.github.com>
2020-04-08 11:06:05 +0200

    mon premier dépot

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>AHHHH ENFIN !!!</code> and save to reload.
+            <code>Hello my friend!</code>
         </p>
         <a
           className="App-link"
